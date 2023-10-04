023b8f43041853be32b54bac27065a03f9c019d0
Dario Alarcon <darioalarcon832@gmail.com>
2023-10-04 13:11:05 -0500

    second commit, function of change language, and clean structure, without the graph

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const languages = {
+    en: {
+        textInput: 'Enter your word',
+        buttomText: 'Go!',
+        aboutRegularExpression: 'Our regular expression is: a(abUabb)^3',
+        historyTittle: 'History',
+        spanishOption: 'Spanish',
+        englishOption: 'English',
+        frenchOption: 'French',
+    },
+    es: {
+        textInput: 'Ingresa tu palabra',
+        buttomText: '¡Vamos!',
+        aboutRegularExpression: 'Nuestra expresión regular es: a(abUabb)^3',
+        historyTittle: 'Historial',
+        spanishOption: 'Español',
+        englishOption: 'Ingles',
+        frenchOption: 'Frances',
+    },
+    fr: {
+        textInput: 'Entrez votre mot',
+        buttomText: 'Allons-y!',
+        aboutRegularExpression: 'notre expression régulière est: a(abUabb)^3',
+        historyTittle: 'Histoire',
+        spanishOption: 'Espagnol',
+        englishOption: 'Anglais',
+        frenchOption: 'Français',
+    },
+};
+
+function changeLanguage(language) {
+    const currentLanguage = languages[language];
+    document.getElementById('word-text').placeholder = currentLanguage.textInput;
+    document.getElementById('word-button').textContent = currentLanguage.buttomText;
+    document.getElementById('English').textContent = currentLanguage.englishOption;
+    document.getElementById('Spanish').textContent = currentLanguage.spanishOption;
+    document.getElementById('French').textContent = currentLanguage.frenchOption;
+    document.getElementById('historyTittle').textContent = currentLanguage.historyTittle;
+    document.getElementById('regularExpressionInfo').textContent = currentLanguage.aboutRegularExpression;
+}
+
+changeLanguage('en');
+
+selectLanguage = document.getElementById('languanges-select')
+selectLanguage.addEventListener('change', function () {
+    selectedLanguage = selectLanguage.value;
+    changeLanguage(selectedLanguage);
+})
+
+
 document.addEventListener("DOMContentLoaded", function() {
     boton = document.getElementById("word-button");
     var input = document.getElementById("word-text");
@@ -18,14 +68,14 @@ document.addEventListener("DOMContentLoaded", function() {
                     var letra = wordToValidate[index];
                     var symbol = document.getElementById("symbol-checking")
                     symbol.innerText = letra;
-                }, i * speed); // Cambia el valor "100" para ajustar la velocidad de escritura
+                }, i * speed);
             })(i);
         }
     }
 
     function get_speed(){
         const slider = document.getElementById("slider");
-        const tiempoSeleccionado = parseFloat(slider.value) * 1000; // Convierte a milisegundos
+        const tiempoSeleccionado = parseFloat(slider.value) * 1000; 
         return tiempoSeleccionado;
     }
 });
