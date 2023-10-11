d4d95e56f3c7836cd2c0469270353207cfd34088
Dario Alarcon <darioalarcon832@gmail.com>
2023-10-11 12:01:30 -0500

    web ready to be reviewed and used has all the requirements except for the commad pattern.

--- script.js
+++ script.js
@@ -38,40 +38,6 @@ class Automaton {
 }
 
 
-
-
-const languages = {
-    en: {
-        textInput: 'Enter your word',
-        buttomText: 'Go!',
-        aboutRegularExpression: 'Our regular expression is: a(abUabb)^3',
-        historyTittle: 'History',
-        spanishOption: 'Spanish',
-        englishOption: 'English',
-        frenchOption: 'French',
-    },
-    es: {
-        textInput: 'Ingresa tu palabra',
-        buttomText: '¡Vamos!',
-        aboutRegularExpression: 'Nuestra expresión regular es: a(abUabb)^3',
-        historyTittle: 'Historial',
-        spanishOption: 'Español',
-        englishOption: 'Ingles',
-        frenchOption: 'Frances',
-    },
-    fr: {
-        textInput: 'Entrez votre mot',
-        buttomText: 'Allons-y!',
-        aboutRegularExpression: 'notre expression régulière est: a(abUabb)^3',
-        historyTittle: 'Histoire',
-        spanishOption: 'Espagnol',
-        englishOption: 'Anglais',
-        frenchOption: 'Français',
-    },
-};
-
-const True = true
-
 const q0  = new State('q0', false);
 const q1  = new State('q1', true);
 const q2  = new State('q2', false);
@@ -115,21 +81,38 @@ const transitions = [
 const states = [q0, q1, q2, q3, q4, q5, q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16];
 const initialState = states.find((state) => state.name === "q0");
 const automaton = new Automaton(states, transitions, initialState);
-function speakResult(isValidateWord){
-    const synth = window.speechSynthesis;
-    var text
-    if(isValidateWord){
-        text = "palabra válida"
-    }else{
-        text = "palabra rechazada"
-    }
-    const utterance = new SpeechSynthesisUtterance(text);
-    
-    utterance.volume = 1; 
-    utterance.rate = 1;   
-    utterance.pitch = 1;  
-    synth.speak(utterance);
-}
+
+
+//************************************************************************************************************************************************* */
+const languages = {
+    en: {
+        textInput: 'Enter your word',
+        buttomText: 'Go!',
+        aboutRegularExpression: 'Our regular expression is: a(abUabb)^3',
+        historyTittle: 'History',
+        spanishOption: 'Spanish',
+        englishOption: 'English',
+        frenchOption: 'French',
+    },
+    es: {
+        textInput: 'Ingresa tu palabra',
+        buttomText: '¡Vamos!',
+        aboutRegularExpression: 'Nuestra expresión regular es: a(abUabb)^3',
+        historyTittle: 'Historial',
+        spanishOption: 'Español',
+        englishOption: 'Ingles',
+        frenchOption: 'Frances',
+    },
+    fr: {
+        textInput: 'Entrez votre mot',
+        buttomText: 'Allons-y!',
+        aboutRegularExpression: 'notre expression régulière est: a(abUabb)^3',
+        historyTittle: 'Histoire',
+        spanishOption: 'Espagnol',
+        englishOption: 'Anglais',
+        frenchOption: 'Français',
+    },
+};
 
 function changeLanguage(language) {
     const currentLanguage = languages[language];
@@ -142,8 +125,38 @@ function changeLanguage(language) {
     document.getElementById('regularExpressionInfo').textContent = currentLanguage.aboutRegularExpression;
 }
 
-changeLanguage('en');
+//******************************************************************************************************************************************************************** */
 
+function speakResult(isValidateWord) {
+    const text = isValidateWord ? "palabra válida" : "palabra rechazada";
+    const utterance = createUtterance(text);
+    speak(utterance);
+  }
+  
+function createUtterance(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    setUtteranceOptions(utterance);
+    return utterance;
+  }
+  
+function setUtteranceOptions(utterance) {
+    utterance.volume = 1;
+    utterance.rate = 1;
+    utterance.pitch = 1;
+  }
+  
+function speak(utterance) {
+    const synth = window.speechSynthesis;
+    synth.speak(utterance);
+  }
+
+//*************************************************************************************************************************************************************************** */
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+//**************************************************************************************************************************************************************** */
 function createHistoryTileItem(userWord){
     const newParagraph = document.createElement('p');
     newParagraph.classList.add('historial-tile'); 
@@ -177,96 +190,41 @@ function createHistoryTile(userWord, isValidate){
     var historyTileSpan = createHistoryTileSpan(isValidate);
     insertSpanInParagraph(historyTile,historyTileSpan);
     insertParagraphIntoDOM(historyTile)
-}
-
-selectLanguage = document.getElementById('languanges-select')
-selectLanguage.addEventListener('change', function () {
-    selectedLanguage = selectLanguage.value;
-    changeLanguage(selectedLanguage);
-})
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    boton = document.getElementById("word-button");
-    var input = document.getElementById("word-text");
-    var hi = highlightLinkBetweenNodes("q9","q10")
-    console.log(hi)
-    boton.addEventListener("click", function() {
-        var valorInput = input.value;
-        var expression = document.getElementById("word-checking")
-        expression.innerText = valorInput
-        input.value = ""
-        processString(valorInput);
-    });
-
-    async function processString(wordToValidate) {
-        automaton.currentState = initialState; 
-        for (let index = 0; index < wordToValidate.length; index++) {
-          const symbolSpan = document.getElementById('symbol-checking')
-          const symbolOrderSpan = document.getElementById('symbol-checking-number')
-          const symbol = wordToValidate[index];
-          const symbolOrder = index + 1;
-          const transition = automaton.transitions.find(
-            (t) => t.fromState === automaton.currentState.name && t.symbol === symbol
-          );
-          showCurrentNodeGraph(automaton.currentState.name)
-          
-          //currentStateElement.innerText = automaton.currentState.name;
-          await sleep(get_speed());
-          symbolSpan.innerText = symbol;
-          symbolOrderSpan.innerText = symbolOrder;
-          if (transition) {
-            var keyNodeTransiction=highlightLinkBetweenNodes(automaton.currentState.name,transition.toState)
-            showCurrentNodeGraph(keyNodeTransiction)
-            //transitionElement.innerText = `${automaton.currentState.name} -> ${transition.toState}`;
-            //currentStateElement.innerText = transition.toState;
-            automaton.currentState = states.find(
-              (state) => state.name === transition.toState
-              );
-            //showCurrentNodeGraph(automaton.currentState.name)
-          } else {
-            //transitionElement.innerText = '';
-            automaton.currentState = initialState; // Si no hay transición, regresamos al estado inicial
-          }
-      
-          await sleep(get_speed());
-      
-          if (!transition) {
-            //console.log(`La cadena "${wordToValidate}" es rechazada`);
-            speakResult(false);
-            createHistoryTile(wordToValidate, false);
-            return;
-          }
-        }
-      
-        console.log(
-          `La cadena "${wordToValidate}" es ${
-            automaton.currentState.isAccepting ? "aceptada" : "rechazada"
-          }`
-        );
-        showCurrentNodeGraph(automaton.currentState.name)
-        speakResult(automaton.currentState.isAccepting);
-        createHistoryTile(wordToValidate, automaton.currentState.isAccepting);
-        
-    }
-
     
-    function get_speed(){
-        const slider = document.getElementById("slider");
-        const tiempoSeleccionado = parseFloat(slider.value) * 1000; 
-        return tiempoSeleccionado;
-    }
-});
-
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  
+    saveToLocalStorage(userWord, isValidate);
 }
 
+function saveToLocalStorage(userWord, isValidate) {
+  const historyData = JSON.parse(localStorage.getItem('historyData')) || [];
+  historyData.push({ userWord, isValidate });
+  localStorage.setItem('historyData', JSON.stringify(historyData));
+}
 
-// Importa la biblioteca GoJS
-//const go = require('gojs');
+function clearLocalStorage() {
+  localStorage.clear();
+}
 
-// Crea un objeto Diagram
+function retrieveHistoryData() {
+  return JSON.parse(localStorage.getItem('historyData')) || [];
+}
+
+function displayHistoryData(historyData) {
+  for (const item of historyData) {
+      createHistoryTile(item.userWord, item.isValidate);
+  }
+}
+
+function initializePage() {
+  const historyData = retrieveHistoryData();
+  clearLocalStorage();
+  displayHistoryData(historyData);
+}
+
+window.addEventListener('load', initializePage);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 const $ = go.GraphObject.make;
 
 const myDiagram = $(go.Diagram, "myDiagram", {
@@ -274,7 +232,6 @@ const myDiagram = $(go.Diagram, "myDiagram", {
     "undoManager.isEnabled": true
 });
 
-// Define un nodo con una propiedad de texto
 myDiagram.nodeTemplate =
     $(go.Node, "Auto",
         {movable: false},
@@ -285,13 +242,12 @@ myDiagram.nodeTemplate =
             new go.Binding("stroke", "color"),
             new go.Binding("fill", "isSelected", (s, obj) => s ? "#BEAEE2" : "white").ofObject()
             ),
-        $(go.TextBlock,  // Vincula el TextBlock a la propiedad "name" del nodo
-            { margin: 10 },  // Ajusta el margen para que el texto no se superponga al borde del nodo
-            new go.Binding("text", "name")  // Vincula la propiedad "name" del nodo al texto del TextBlock
+        $(go.TextBlock, 
+            { margin: 10 },  
+            new go.Binding("text", "name")  
         )
     );
 
-// Añade tres nodos al grafo con textos diferentes
 myDiagram.model.addNodeData({ key: "_",  name: "" , color: "transparent", loc: "-1100 -10" });
 myDiagram.model.addNodeData({ key: "q0",  name: "q0" , color: "purple", loc: "-1030 -10" });
 myDiagram.model.addNodeData({ key: "q1",  name: "q1" , color: "red",   loc: "-960 -10"  });
@@ -309,8 +265,6 @@ myDiagram.model.addNodeData({ key: "q12", name: "q12", color: "purple", loc: "-7
 myDiagram.model.addNodeData({ key: "q13", name: "q13", color: "red",   loc: "-715 150"  });
 myDiagram.model.addNodeData({ key: "q14", name: "q14", color: "purple", loc: "-645 200"  });
 myDiagram.model.addNodeData({ key: "q15", name: "q15", color: "red",   loc: "-575 250"  });
-
-
 
 myDiagram.model.addNodeData({ key: "T1", color:"transparent", loc: "-995 -10"  });
 myDiagram.model.addNodeData({ key: "T2", color:"transparent", loc: "-925 -10"  });
@@ -331,7 +285,7 @@ myDiagram.model.addNodeData({ key: "T16", color:"transparent", loc: "-697 40"  }
 myDiagram.model.addNodeData({ key: "T17", color:"transparent", loc:  "-695 70" });
 myDiagram.model.addNodeData({ key: "T18", color:"transparent",  loc: "-522 90"  });
 myDiagram.model.addNodeData({ key: "T19", color:"transparent", loc:  "-520 120" });
-// Define un enlace
+
 myDiagram.linkTemplate =
     $(go.Link,
         $(go.Shape, { 
@@ -339,8 +293,8 @@ myDiagram.linkTemplate =
         new go.Binding("stroke", "isHighlighted", fla => fla ? "red" : "gray")
     ),
         $(go.Shape, { toArrow: "Standard" }),
-        $(go.Panel, "Auto",  // visual hint that the user can do something with this link label
-          $(go.Shape,  // the label background, which becomes transparent around the edges
+        $(go.Panel, "Auto",  
+          $(go.Shape,  
             {
               fill: $(go.Brush, "Radial",
                 { 0: "rgb(240, 240, 240)", 0.3: "rgb(240, 240, 240)", 1: "rgba(240, 240, 240, 0)" }),
@@ -353,17 +307,14 @@ myDiagram.linkTemplate =
               textAlign: "center",
               font: "10pt helvetica, arial, sans-serif",
               stroke: "black",
-              margin: 4, // editing the text automatically updates the model data
+              margin: 4, 
             },
             new go.Binding("text", "text").makeTwoWay()),
-          // The GraphObject.segmentOffset property is what the LinkLabelDraggingTool modifies.
-          // This TwoWay binding saves any changes to the same named property on the link data.
           new go.Binding("segmentOffset", "segmentOffset", go.Point.parse).makeTwoWay(go.Point.stringify)
         )
             
     );
 
-// Añade enlaces al grafo
 myDiagram.model.addLinkData({ from: "_",  to: "q0"  });
 myDiagram.model.addLinkData({ from: "q0",  to: "q1"  , text: "a", key:"T1"});
 myDiagram.model.addLinkData({ from: "q1",  to: "q2"  , text: "b", key:"T2"});
@@ -386,27 +337,109 @@ myDiagram.model.addLinkData({ from: "q14", to: "q9"  , text: "b", key:"T18"});
 myDiagram.model.addLinkData({ from: "q8",  to: "q15" , text: "a", key:"T19"});
 
 
-
-
-async function showCurrentNodeGraph(key){
+async function showCurrentNodeGraph(key) {
     const currentNode = myDiagram.findNodeForKey(key);
-    currentNode.isSelected = true
-    await sleep(1000)
-    currentNode.isSelected = false
-}
+    selectNode(currentNode);
+    await sleep(1000);
+    deselectNode(currentNode);
+  }
+  
+  function selectNode(node) {
+    node.isSelected = true;
+  }
+  
+  function deselectNode(node) {
+    node.isSelected = false;
+  }
 
 
 function highlightLinkBetweenNodes(startNodeKey, endNodeKey) {
     const linkDataArray = myDiagram.model.linkDataArray;
-  
-    // Busca el enlace que conecta los nodos de inicio y final
     for (let i = 0; i < linkDataArray.length; i++) {
       const linkData = linkDataArray[i];
       console.log(linkData)
       if (linkData.from === startNodeKey && linkData.to === endNodeKey) {
-        // Encontramos el enlace, así que lo resaltamos
-        return linkData.key; // Actualiza la vista del diagrama
-         // Sal del bucle una vez que hayas encontrado el enlace
+        return linkData.key; 
       }
     }
   }
+/************************************************************************************************************************************************************************** */
+const selectLanguageDom = document.getElementById('languanges-select')
+
+selectLanguageDom.addEventListener('change', handleLanguageChange )
+
+function handleLanguageChange(){
+    var selectedLanguage = selectLanguageDom.value;
+    changeLanguage(selectedLanguage);
+}
+
+function get_speed(){
+    const slider = document.getElementById("slider");
+    const tiempoSeleccionado = parseFloat(slider.value) * 1000; 
+    return tiempoSeleccionado;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeLanguage('en');
+    var botton = document.getElementById("word-button");
+    var input = document.getElementById("word-text");
+
+
+    botton.addEventListener("click", handleButtonClick);
+
+    function handleButtonClick() {
+        const valorInput = input.value;
+        displayExpression(valorInput);
+        clearInput();
+        processString(valorInput);
+      }
+      
+    function displayExpression(expression) {
+        const expressionElement = document.getElementById('word-checking');
+        expressionElement.innerText = expression;
+      }
+      
+    function clearInput() {
+        input.value = '';
+      }
+
+      async function processString(wordToValidate) {
+        automaton.currentState = initialState; 
+        for (let index = 0; index < wordToValidate.length; index++) {
+          const symbolSpan = document.getElementById('symbol-checking')
+          const symbolOrderSpan = document.getElementById('symbol-checking-number')
+          const symbol = wordToValidate[index];
+          const symbolOrder = index + 1;
+          const transition = automaton.transitions.find(
+            (t) => t.fromState === automaton.currentState.name && t.symbol === symbol
+          );
+          showCurrentNodeGraph(automaton.currentState.name)
+          await sleep(get_speed());
+          symbolSpan.innerText = symbol;
+          symbolOrderSpan.innerText = symbolOrder;
+          if (transition) {
+            var keyNodeTransiction=highlightLinkBetweenNodes(automaton.currentState.name,transition.toState)
+            showCurrentNodeGraph(keyNodeTransiction)
+            automaton.currentState = states.find(
+              (state) => state.name === transition.toState
+              );
+          } else {
+            automaton.currentState = initialState;
+          }
+      
+          await sleep(get_speed());
+      
+          if (!transition) {
+            speakResult(false);
+            createHistoryTile(wordToValidate, false);
+            return;
+          }
+        }
+      
+        showCurrentNodeGraph(automaton.currentState.name)
+        speakResult(automaton.currentState.isAccepting);
+        createHistoryTile(wordToValidate, automaton.currentState.isAccepting);
+        
+    }
+      
+});
